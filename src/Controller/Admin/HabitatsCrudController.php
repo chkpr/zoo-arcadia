a86e5c624626a5586057cfdcc30dd3ae853c89af
chk363 <gitchkip@aol.com>
2024-11-23 17:51:46 +0100

    add image upload on admin dashboard

--- src/Controller/Admin/HabitatsCrudController.php
+++ src/Controller/Admin/HabitatsCrudController.php
@@ -5,9 +5,11 @@ namespace App\Controller\Admin;
 use App\Entity\Habitats;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class HabitatsCrudController extends AbstractCrudController
 {
@@ -19,8 +21,15 @@ class HabitatsCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
+        $mappingsParams = $this->getParameter('vich_uploader.mappings');
+
+        $habitatImagePath = $mappingsParams['habitat']['uri_prefix'];
+
         yield TextField::new('name', 'Nom');
-        yield TextareaField::new('Description', 'Description');
+        yield TexteditorField::new('Description', 'Description');
+        yield ImageField::new('imageName', 'Image')->setBasePath($habitatImagePath)->hideOnForm();
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex();
+
     }
     
 }
--- src/Entity/Habitats.php
+++ src/Entity/Habitats.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: HabitatsRepository::class)]
+#[Vich\Uploadable]
 class Habitats
 {
     #[ORM\Id]
@@ -27,6 +30,18 @@ class Habitats
      */
     #[ORM\OneToMany(targetEntity: Animals::class, mappedBy: 'habitat')]
     private Collection $animals;
+
+    #[Vich\UploadableField(mapping: 'habitat', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function __construct()
     {
@@ -95,5 +110,50 @@ class Habitats
         }
 
         return $this;
+    }
+
+        /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
     }
 }
